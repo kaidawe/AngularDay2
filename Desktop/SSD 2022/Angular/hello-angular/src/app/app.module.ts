a0d12c6e4e817acbbb3659e0baa3765330e30847
Haley Dawe <hdawe@my.bcit.ca>
2023-03-02 09:46:30 -0800

    day 3 morning push

--- Desktop/SSD 2022/Angular/hello-angular/src/app/app.module.ts
+++ Desktop/SSD 2022/Angular/hello-angular/src/app/app.module.ts
@@ -3,9 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ProjectFilterPipe } from './project-filter.pipe';
+import { ProjectComponent } from './projects/projects.component';
+import { TagsComponent } from './tags/tags.component';
+import { CategoryComponent } from './categories/categories.component';
+import { ProjectDetailComponent } from './project-detail/project-detail.component';
 
 @NgModule({
-  declarations: [AppComponent, ProjectFilterPipe],
+  declarations: [AppComponent, ProjectFilterPipe, ProjectComponent, TagsComponent, CategoryComponent, ProjectDetailComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ Desktop/SSD 2022/Angular/hello-angular/src/app/data/categories.ts
@@ -0,0 +1,7 @@
+import { Category } from "../model/category";
+
+export const CATEGORIES: Category[] = [
+    { id: 1, name: 'Web Development', slug: 'web-development' },
+    { id: 2, name: 'Graphic Design', slug: 'graphic-design' },
+    { id: 3, name: 'Photography', slug: 'photography' },
+  ];
